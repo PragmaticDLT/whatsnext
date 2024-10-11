248e86dc792ac3f919e1e198cd3693b5dd997f69
Matias Leiva <matiasoscarleiva@gmail.com>
2024-10-11 10:48:31 -0300

    test deploy digitalocean

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -21,28 +21,6 @@ export default function Header() {
               What's Next Life Coach
             </h1>
           </div>
-
-          {/* <div className="flex">
-            <button
-              className="text-slate-500 hover:text-slate-600"
-              aria-controls="sidebar"
-              aria-expanded={sidebarOpen}
-              onClick={() => {
-                setSidebarOpen(!sidebarOpen);
-              }}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg
-                className="w-6 h-6 fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="4" y="5" width="16" height="2" />
-                <rect x="4" y="11" width="16" height="2" />
-                <rect x="4" y="17" width="16" height="2" />
-              </svg>
-            </button>
-          </div> */}
         </div>
       </div>
     </header>
